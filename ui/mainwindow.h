cf0d1ac0ccfb14138b44f621c8fea087924ed4e6
paulwrodriguez <paul.rodriguez@ucdenver.edu>
2015-10-12 17:20:21 -0600

    init commit for ui

---
+++ ui/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <string>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_Browse_clicked();
+    void on_Cancel_clicked();
+
+    void on_Start_clicked();
+
+    void on_radioButton_toggled(bool checked);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H

b1e86f5a2d0d1601e8f2c0fe01c309a4694230ce
nicole-cranon <nicole.cranon@gmail.com>
2015-10-18 10:10:29 -0600

    added header file for oni-to-pcd.h

---
+++ include/oni-to-pcd.h
@@ -0,0 +1,67 @@
+/*	
+Team:			VolcanoBot A
+Project:		TBD
+File:			oni-to-pcd.h
+Description:	Reads an oni file recorded using the Openni2 library and outputs point clouds (pcd files)
+*/
+
+#ifndef _ONI_TO_PCD
+#define _ONI_TO_PCD
+
+#include <vector>
+#include "pcl/io/openni2_grabber.h"
+#include "pcl/point_cloud.h"
+
+/*
+oni to pcd class
+*/
+class oni2pcd {
+public:
+	/*
+	constructors
+	*/
+	oni2pcd () : oniFilesBeingRead(), 
+		pcdWriteDir(),
+		totalFrames(0),
+		framesToRead(0),
+		currentFrame(0),
+		frameSkip(0) {} 
+
+	/*
+	read single oni and write pcds
+	*/
+	void readOni (const char* const oniFile, 
+		const int framesToSkip = DEFAULT_FRAME_SKIP);
+
+	// /*
+	// read multiple oni files
+	// */
+	// void readOnis (const std::vector<const char*>  oniFiles[], 
+	// 	const int framesToSkip = DEFAULT_FRAME_SKIP);
+
+	// /*
+	// get oni files being read
+	// */
+	// const std::vector<const char*> getOniFilesBeingRead () const;
+
+	static const int DEFAULT_FRAME_SKIP = 25;
+
+private:
+	const std::vector<const char*> oniFilesBeingRead;
+	const std::vector<const char*> pcdWriteDir;
+
+	int totalFrames,
+		framesToRead,
+		currentFrame,
+		frameSkip;
+
+	typedef pcl::PointCloud<pcl::PointXYZRGBA> Cloud;
+	typedef Cloud::ConstPtr CloudConstPtr;
+
+	void writeCloudCb (const CloudConstPtr& cloud);
+
+	void setFrameSkip (const int framesToSkip);
+
+};
+
+#endif
--- src/oni-to-pcd.cpp
+++ src/oni-to-pcd.cpp
@@ -1,18 +1,41 @@
-/*	Team:			VolcanoBot A
-	File:			oni-to-pcd
-	Description:	Reads an oni file recorded using the Openni2 library and outputs point clouds (pcd files)
+/*	
+Team:			VolcanoBot A
+Project:		TBD
+File:			oni-to-pcd.cpp
+Description:	Reads an oni file recorded using the Openni2 library and outputs point clouds (pcd files)
 */
 
-#include "openni2_grabber.h"
+#include "oni-to-pcd.h"
+#include "pcl/point_types.h"
 #include <iostream>
 #include <cstdlib>
 
 int main (int argc, char* argv[]) {
-	// expect 2 additional arguments, ie. <executable> <oniFile> <pcdWriteDir>
+	/*
+	expect 2 additional arguments, ie. <executable> <oniFile> <pcdWriteDir>
+	*/
 	const int REQ_ARGS = 3;
 	if (argc < REQ_ARGS) {
-
+ 		std::cout << "\nNot enough arguments provided.\n";
+ 		return EXIT_FAILURE;
 	}
 
+
+
 	return EXIT_SUCCESS;
+}
+
+/*
+public members of oni2pcd
+*/
+void oni2pcd::readOni (const char* const oniFile, 
+		const int framesToSkip) {
+
+}
+
+/*
+private members of oni2pcd
+*/
+void oni2pcd::writeCloudCb (const oni2pcd::CloudConstPtr& cloud) {
+
 }

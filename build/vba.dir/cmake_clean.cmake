0d716ecab505b390d4923e7d80fe1e499dce25db
paulwrodriguez <paul.rodriguez@ucdenver.edu>
2015-10-29 19:00:56 -0600

    Revert "Develop"

---
+++ build/vba.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/../vba.dir/main.cpp.o"
+  "../vba.pdb"
+  "../vba"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/../vba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
